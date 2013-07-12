c291035e559b69940fdd64720ebed663b061d1d8
Zachery croasmun <th3p4ler1der@gmail.com>
2013-07-11 20:10:07 -0700

    last commit for today

--- js/json.js
+++ js/json.js
@@ -9,14 +9,47 @@ var json = {
 		"type": ["Drink Type:", "Alcoholic" ],
 		"date": ["date:", "05/09/2005"],
 		"note": ["note:", "this is the best drink"]
-		}
-	/*"drinks1": {
+		
+	},
+		
+		
+	"drinks1": {
 		"name": ["Drink Name:", "pepsi and jack"],
 		"type": ["Drink Type:", "Alcoholic" ],
 		"date": ["date:", "03/11/2011"],
 		"note": ["note:", "this is a great drink"]
 		
-	}*/
+	},
+	
+	"drinks2": {
+		"name": ["Drink Name:", "pepsi"],
+		"type": ["Drink Type:", "Non-alcoholic" ],
+		"date": ["date:", "03/11/2011"],
+		"note": ["note:", "this is a great drink"]
+		
+	},
+	
+	"drinks3": {
+		"name": ["Drink Name:", "coke"],
+		"type": ["Drink Type:", "Non-alcoholic" ],
+		"date": ["date:", "03/11/2011"],
+		"note": ["note:", "this is a great drink"]
+		
+	},
+	"drinks4": {
+		"name": ["Drink Name:", "jack and coke"],
+		"type": ["Drink Type:", "Alcoholic" ],
+		"date": ["date:", "03/11/2011"],
+		"note": ["note:", "this is a great drink"]
+		
+	},
+	"drinks5": {
+		"name": ["Drink Name:", "sunrise"],
+		"type": ["Drink Type:", "Alcoholic" ],
+		"date": ["date:", "03/11/2011"],
+		"note": ["note:", "this is a great drink"]
+		
+	},
 	
 	
 	
